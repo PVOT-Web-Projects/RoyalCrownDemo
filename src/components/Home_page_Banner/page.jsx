03c611d5096f5ee730507fcc53d4da6751be3225
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-10-16 11:47:44 +0530

    sdsd

--- src/components/Home_page_Banner/page.jsx
+++ src/components/Home_page_Banner/page.jsx
@@ -1,116 +1,43 @@
 import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-// import Lenis from "@studio-freight/lenis"; // Import Lenis
-// import MusicPlayer from "@/Components/musicPlayer/page";
 import { motion, useAnimation } from "framer-motion";
-// import HeadingTextAnimation from "@/components/AnimatedText/HeadingTextAnimation";
 import { useInView } from "react-intersection-observer";
 import styles from "@/components/Home_page_Banner/Banner.module.css";
-import "./scroll.css";
-import Link from "next/link";
 import Popup from "@/components/Popup/page";
-import { errorToJSON } from "next/dist/server/render";
-// import Image from "next/image";
-// import Img1 from "@/images/livingroom.svg"
+
 gsap.registerPlugin(ScrollTrigger);
 
 const Animation = ({ loadImage, counter }) => {
-  const [info, setInfo] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [activeTab, setActiveTab] = useState(0);
-  // const [animationEnded, setAnimationEnded] = useState(false);
   const sectionRef = useRef(null);
   const canvasRef = useRef(null);
-  const textRef = useRef(null);
   const contextRef = useRef(null);
   const imagesRef = useRef([]);
   const airpodsRef = useRef({ frame: 0 });
-  const [loading, setLoading] = useState(true);
-  const [loadingCounter, setLoadingCounter] = useState(0);
-  const [scrollPercentage, setScrollPercentage] = useState(0);
-  const [isVisible, setIsVisible] = useState(true);
   const [popup, setPopup] = useState(false);
-  const [frameCount, setFrameCount] = useState(0);
-  const [imageBaseUrl, setImageBaseUrl] = useState("");
-  const handlePopup = () => {
-    setPopup(true);
-  };
-  const handlePopupClose = () => {
-    setPopup(false);
-  };
-  console.log(loadingCounter);
-  // Define frame counts and base URLs for each tab
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
   const tabs = [
     {
-      frameCount: 290,
-      imageBaseUrl:
-        "https://interiormaataassets.humbeestudio.xyz/mainsiteassets/desktop/",
+      frameCount: 300,
+      imageBaseUrl: "https://interiormaataassets.humbeestudio.xyz/mainsiteassets/desktop/",
     },
     {
-      frameCount: 250,
-      imageBaseUrl:
-        "https://interiormaataassets.humbeestudio.xyz/mainsiteassets/desktop/",
+      frameCount: 220,
+      imageBaseUrl: "https://interiormaataassets.humbeestudio.xyz/mainsiteassets/bedroom/",
     },
     {
       frameCount: 300,
-      imageBaseUrl:
-        "https://interiormaataassets.humbeestudio.xyz/mainsiteassets/desktop/",
+      imageBaseUrl: "https://interiormaataassets.humbeestudio.xyz/mainsiteassets/desktop/",
     },
   ];
-  useEffect(() => {
-    const defaultTabIndex = 0; // Set default tab index
-    setActiveTab(defaultTabIndex);
-    setFrameCount(tabs[defaultTabIndex].frameCount);
-    setImageBaseUrl(tabs[defaultTabIndex].imageBaseUrl);
-    loadImages(tabs[defaultTabIndex].frameCount, tabs[defaultTabIndex].imageBaseUrl);
-  }, []);
-  const handleTabChange = (index) => {
-    console.log(`Tab clicked: ${index}`); // Log the clicked tab index
-    setActiveTab(index);
-    setFrameCount(tabs[index].frameCount);
-    setImageBaseUrl(tabs[index].imageBaseUrl);
-    imagesRef.current = []; // Clear previous images
-    setLoading(true); // Reset loading state
-    // Reset the frame to 0 for the new tab
-  airpodsRef.current.frame = 0; // Reset frame to the first frame
-  loadImages(tabs[index].frameCount, tabs[index].imageBaseUrl); // Load images for the new tab
-  };
-  // const loadImages = async (frameCount, baseUrl) => {
-  //   const imgUrls = Array.from(
-  //     { length: frameCount },
-  //     (_, i) => `${baseUrl}${(i + 1).toString().padStart(4, "0")}.webp`
-  //   );
-  //   try {
-  //     await Promise.all(
-  //       imgUrls.map(
-  //         (url, index) =>
-  //           new Promise((resolve) => {
-  //             const img = new Image();
-  //             img.src = url;
-  //             img.onload = () => {
-  //               imagesRef.current.push(img);
-  //               setLoadingCounter(index + 1);
-  //               resolve();
-  //             };
-  //             img.onerror = () => {
-  //               console.error(`Error loading image: ${url}`);
-  //               resolve(); // Resolve even on error to continue loading others
-  //             };
-  //           })
-  //       )
-  //     );
-  //     setLoading(false);
-  //   } catch (error) {
-  //     console.error("Error loading images:", error);
-  //   }
-  // };
 
   const loadImages = async (frameCount, baseUrl) => {
-    const imgUrls = Array.from(
-      { length: frameCount },
-      (_, i) => `${baseUrl}${(i + 1).toString().padStart(4, "0")}.webp`
-    );
-    setLoading(true); // Set loading to true immediately
+    const imgUrls = Array.from({ length: frameCount }, (_, i) => `${baseUrl}${(i + 1).toString().padStart(4, "0")}.webp`);
+
+    setLoading(true);
     
     const promises = imgUrls.map((url) => {
       return new Promise((resolve) => {
@@ -126,14 +53,25 @@ const Animation = ({ loadImage, counter }) => {
         };
       });
     });
-  
+
     await Promise.all(promises);
-    // Reset to the first frame once images are loaded
-  airpodsRef.current.frame = 0; 
-  render(); // Call render to display the first image
+    airpodsRef.current.frame = 0; 
+    requestAnimationFrame(render);
     setLoading(false);
   };
-  
+
+  useEffect(() => {
+    const defaultTabIndex = 0;
+    setActiveTab(defaultTabIndex);
+    loadImages(tabs[defaultTabIndex].frameCount, tabs[defaultTabIndex].imageBaseUrl);
+  }, []);
+
+  const handleTabChange = (index) => {
+    setActiveTab(index);
+    imagesRef.current = []; // Clear previous images
+    loadImages(tabs[index].frameCount, tabs[index].imageBaseUrl);
+  };
+
   useEffect(() => {
     const section = sectionRef.current;
     const canvas = canvasRef.current;
@@ -141,182 +79,73 @@ const Animation = ({ loadImage, counter }) => {
     contextRef.current = context;
 
     const setCanvasSize = () => {
-      // const originalWidth = 1632;
-      // const originalHeight = 918;
-      // const aspectRatio = originalWidth / originalHeight;
-      // const availableWidth = window.innerWidth;
       const aspectRatio = 1632 / 918;
       const availableWidth = window.innerWidth;
 
       if (availableWidth < 200) {
         canvas.width = 1632 / 2;
         canvas.height = 918 / 2;
-        canvas.style.width = "1301px";
-        canvas.style.height = "100vh";
       } else {
         canvas.width = 1632;
         canvas.height = 918;
-        canvas.style.width = "100%";
-        canvas.style.height = "100vh";
       }
+
+      canvas.style.width = "100%";
+      canvas.style.height = "100vh";
     };
+
     setCanvasSize();
     window.addEventListener("resize", setCanvasSize);
-    // https://interiormaataassets.humbeestudio.xyz/mainsiteassets/desktop/0001.webp
-    const frameCount = 290;
-    const currentFrame = (index) =>
-      `https://interiormaataassets.humbeestudio.xyz/mainsiteassets/desktop/${(
-        index + 1
-      )
-        .toString()
-        .padStart(4, "0")}.webp`;
-
-    // let imgL = [];
-    // for (let i = 0; i < frameCount; i++) {
-    //   let img = new Image();
-    //   img.src = currentFrame(i);
-    //   imagesRef.current.push(img);
-    //   imgL.push(img.src);
-    // }
-
-    // const loadImages = async () => {
-    //   try {
-    //     const loadImagePromises = imgL.map((imageUrl, index) => {
-    //       return new Promise((resolve) => {
-    //         const img = new Image();
-    //         img.src = imageUrl;
-    //         img.onload = () => {
-    //           setLoadingCounter(index + 1);
-    //           resolve();
-    //         };
-    //       });
-    //     });
-
-    //     await Promise.all(loadImagePromises);
-    //     setLoading(false);
-    //   } catch (error) {
-    //     console.error("Error loading images:", error);
-    //   }
-    // };
-    // loadImages();
-    // console.log(imgL);
-    // console.log("Counter", loadingCounter);
 
     const animationTimeline = gsap.timeline({
-      onUpdate: render,
-      // onComplete: () => setAnimationEnded(true),
       scrollTrigger: {
         trigger: section,
         pin: true,
-        scrub: false, // Increase scrub value for smoother transitions effect it will take 2 seconds to scroll use true for default effect
-        //       smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
-        // effects: true, // looks for data-speed and data-lag attributes on elements
-        // smoothTouch: 100,
+        scrub: false,
         end: "+=1400%",
         onUpdate: (self) => {
           const progress = self.progress;
-          airpodsRef.current.frame = Math.floor(progress * (frameCount - 1));
-          render();
+          airpodsRef.current.frame = Math.floor(progress * (tabs[activeTab].frameCount - 1));
+          requestAnimationFrame(render);
         },
       },
     });
-    // Check if there are images loaded before setting the onload
-    if (imagesRef.current.length > 0) {
-      imagesRef.current[0].onload = render;
-    }
-
-    function render() {
-      if (imagesRef.current[airpodsRef.current.frame]) {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(
-          imagesRef.current[airpodsRef.current.frame],
-          0,
-          0,
-          canvas.width,
-          canvas.height
-        );
-      }
-    }
-    // animationTimeline.to(airpodsRef.current, {
-    //   frame: frameCount - 1,
-    //   snap: "frame",
-    //   ease: "none",
-    //   duration: 1,
-    // });
-
-    // imagesRef.current[0].onload = render;
-
-    function render() {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.drawImage(
-        imagesRef.current[airpodsRef.current.frame],
-        0,
-        0,
-        canvas.width,
-        canvas.height
-      );
-    }
 
     return () => {
       window.removeEventListener("resize", setCanvasSize);
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
-  }, [frameCount, loadingCounter]);
+  }, [activeTab]);
 
-  console.log(loadImage(loading));
-
-  const [ref, inView] = useInView({
-    triggerOnce: false,
-  });
-  const controls = useAnimation();
-
-  useEffect(() => {
-    if (inView) {
-      controls.start("visible");
+  const render = () => {
+    if (imagesRef.current[airpodsRef.current.frame]) {
+      contextRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+      contextRef.current.drawImage(
+        imagesRef.current[airpodsRef.current.frame],
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
     }
-  }, [controls, inView]);
+  };
 
-  useEffect(() => {
-    const updateScrollPercentage = () => {
-      const scrollPosition = window.scrollY;
-      const windowHeight = window.innerHeight;
-      const documentHeight = document.documentElement.scrollHeight;
-      const totalScroll = documentHeight - windowHeight;
-      const currentScrollPercentage = (scrollPosition / totalScroll) * 100;
-      setScrollPercentage(currentScrollPercentage);
-    };
-
-    window.addEventListener("scroll", updateScrollPercentage);
-
-    return () => window.removeEventListener("scroll", updateScrollPercentage);
-  }, []);
-
-  const loadingProgress = (loadingCounter / 250) * 100;
-  console.log(counter(loadingProgress));
-  const scrollDownByTenPercent = () => {
-    const tenPercentOfHeight = window.innerHeight * 1.7;
-    window.scrollBy({
-      top: tenPercentOfHeight,
-      behavior: "smooth",
-    });
-    setIsVisible(false);
+  const updateScrollPercentage = () => {
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+    const totalScroll = documentHeight - windowHeight;
+    const currentScrollPercentage = (scrollPosition / totalScroll) * 100;
+    setScrollPercentage(currentScrollPercentage);
   };
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsVisible(false);
-      } else {
-        setIsVisible(true);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    window.addEventListener("scroll", updateScrollPercentage);
+    return () => window.removeEventListener("scroll", updateScrollPercentage);
   }, []);
+
+  const handlePopup = () => setPopup(true);
+  const handlePopupClose = () => setPopup(false);
 
   const [buttonRef, buttonInView] = useInView();
 
@@ -335,121 +164,29 @@ const Animation = ({ loadImage, counter }) => {
           style={{
             width: "100%",
             height: "100vh",
-            willChange: "transform", // Hint the browser for optimization
+            willChange: "transform",
           }}
         ></canvas>
-         {/* {loading && <div className={styles.loadingPlaceholder}>Loading...</div>} */}
+        {loading && <div className={styles.loadingPlaceholder}>Loading...</div>}
       </section>
-      {/* <motion.div
-        ref={ref}
-        initial="hidden"
-        animate={inView ? "visible" : "hidden"}
-        transition={{ duration: 0.9 }}
-        className={styles.text1}
-      >
-        <HeadingTextAnimation
-          heading={"The"}
-          justifyContent={"center"}
-        />
-        <HeadingTextAnimation
-          heading={"Crown"}
-          justifyContent={"center"}
-        />
-         <HeadingTextAnimation
-          heading={"Experience"}
-          justifyContent={"center"}
-        />
-      </motion.div> */}
 
-      {/* <motion.div
-        ref={ref}
-        initial="hidden"
-        animate={inView ? "visible" : "hidden"}
-        transition={{ duration: 0.9 }}
-        className={styles.interiormaata}
-      >
-        <HeadingTextAnimation
-          heading={"interior माता"}
-          justifyContent={"center"}
-        />
-      </motion.div> */}
-
-      {/* <div class="scroll-down-wrap no-border">
-        {isVisible && (
-          <div className="section-down-arrow" onClick={scrollDownByTenPercent}>
-            <svg
-              class="nectar-scroll-icon"
-              viewBox="0 0 30 45"
-              enableBackground="new 0 0 30 45"
-            >
-              <path
-                class="nectar-scroll-icon-path"
-                fill="none"
-                stroke="#ffffff"
-                strokeWidth="2"
-                strokeMiterlimit="10"
-                d="M15,1.118c12.352,0,13.967,12.88,13.967,12.88v18.76  c0,0-1.514,11.204-13.967,11.204S0.931,32.966,0.931,32.966V14.05C0.931,14.05,2.648,1.118,15,1.118z"
-              ></path>
-            </svg>
-            <div className="scroll">Scroll to explore</div>
-          </div>
-        )}
-      </div> */}
-
-      {/* <video
-        className={styles.videoBg}
-        width="750"
-        height="500"
-        autoPlay
-        loop
-        muted
-      >
-        <source src="./video/final_frame_video.mp4" type="video/mp4" />
-      </video> */}
-
-      {/* <MusicPlayer /> */}
-      {scrollPercentage >= 10 && (
+      {scrollPercentage >= 3 && (
         <div className={styles.buttonOuter} ref={buttonRef}>
-          <div>
-            <motion.div
-              className={styles.buttonX}
-              role="button"
-              initial="hidden"
-              animate={
-                window.innerWidth < 768
-                  ? scrollPercentage >= 55
-                    ? "upsideDown"
-                    : "visible"
-                  : window.innerWidth < 1024
-                  ? scrollPercentage >= 62
-                    ? "upsideDown"
-                    : "visible"
-                  : window.innerWidth < 1400
-                  ? scrollPercentage >= 65
-                    ? "upsideDown"
-                    : "visible"
-                  : window.innerWidth < 500
-                  ? scrollPercentage >= 58
-                    ? "upsideDown"
-                    : "visible"
-                  : window.innerWidth < 1600
-                  ? scrollPercentage >= 60
-                    ? "upsideDown"
-                    : "visible"
-                  : scrollPercentage >= 63
-                  ? "upsideDown"
-                  : "visible"
-              }
-              variants={buttonVariants}
-            >
-              {/* <div className={styles.tabContainer}> */}
-                {tabs.map((tab, index) => (
-                  <div
-                    className={styles.textX}
-                    key={index}
-                    onClick={() => handleTabChange(index)}
-                  >
-                    <svg
+          <motion.div
+            className={styles.buttonX}
+            role="button"
+            initial="hidden"
+            animate={scrollPercentage >= 55 ? "upsideDown" : "visible"}
+            variants={buttonVariants}
+          >
+            {tabs.map((tab, index) => (
+              <div
+                className={styles.textX}
+                key={index}
+                onClick={() => handleTabChange(index)}
+              >
+                {/* SVG Icon Here */}
+                <svg
                       width="48"
                       height="60"
                       viewBox="0 0 48 60"
@@ -482,14 +219,12 @@ const Animation = ({ loadImage, counter }) => {
                         strokeLinecap="round"
                       />
                     </svg>
-                  </div>
-                ))}
-              {/* </div> */}
-            </motion.div>
-          </div>
+              </div>
+            ))}
+          </motion.div>
         </div>
       )}
-      {/* info button  */}
+
       {scrollPercentage >= 3 && (
         <div className={styles.buttonOuter} ref={buttonRef}>
           <div className={styles.ModalPopupOuter}>
@@ -501,35 +236,11 @@ const Animation = ({ loadImage, counter }) => {
               className={styles.buttonXInner}
               role="button"
               initial="hidden"
-              animate={
-                window.innerWidth < 768
-                  ? scrollPercentage >= 55
-                    ? "upsideDown"
-                    : "visible"
-                  : window.innerWidth < 1024
-                  ? scrollPercentage >= 62
-                    ? "upsideDown"
-                    : "visible"
-                  : window.innerWidth < 1400
-                  ? scrollPercentage >= 65
-                    ? "upsideDown"
-                    : "visible"
-                  : window.innerWidth < 500
-                  ? scrollPercentage >= 58
-                    ? "upsideDown"
-                    : "visible"
-                  : window.innerWidth < 1600
-                  ? scrollPercentage >= 60
-                    ? "upsideDown"
-                    : "visible"
-                  : scrollPercentage >= 63
-                  ? "upsideDown"
-                  : "visible"
-              }
+              animate={scrollPercentage >= 55 ? "upsideDown" : "visible"}
               variants={buttonVariants}
             >
               <div className={styles.textX}>
-                <svg
+              <svg
                   width="24"
                   height="24"
                   xmlns="http://www.w3.org/2000/svg"
